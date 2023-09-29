665da406abd8e0ec3d815fc16f362d360b501c25
upendrapanwar <upendrapanwar456@gmail.com>
2023-09-29 16:31:05 +0530

    uploading node latest files 29-09-2023

--- app/services/common.service.js
+++ app/services/common.service.js
@@ -159,76 +159,13 @@ async function authenticate({ email, password, role }) {
  * @returns Object|null
  */
 async function subscription(param) {
-    /*const reader = new window.FileReader();
-    console.log('param.certificate=',param.certificate)
-    var files = reader.readAsDataURL(param.certificate);
-    let extArray = files.split(".");
-    let extension = extArray[extArray.length - 1];
-    
-    var path_temp = param.certificate;
-    var filename = 'certificate' + "-" + Math.floor(Math.random() * 10000) + "-" + Date.now() + "." + extension;
-    fs.move(path_temp, config.uploadDir + '/' + filename, function(err) {
-        if (err) return console.error(err)
-        console.log("file uploaded!")
-    });*/
     //console.log("param",param);
     var whereCondition = { _id: param.uid};
     const test = User.findOne(whereCondition);
     console.log('test',test);
+    
     if (await User.findOne(whereCondition)) {
-        const user = new User({
-            firstname: param.firstname,
-            surname: param.surname,
-            id_number: param.id_number,
-            subscriber_email: param.email,
-            mobile_number: param.mobile_number,
-            alternate_mobile_number: param.alternate_mobile_number,
-            street: param.street,
-            street_name: param.street_name,
-            complex_n_unit: param.complex_n_unit,
-            suburb_district: param.suburb_district,
-            town_city: param.town_city,
-            province: param.province,
-            postal_code: param.postal_code,
-            payment_option: param.payment_option,
-            account_holder_title: param.account_holder_title,
-            account_holder_name: param.account_holder_name,
-            account_holder_surname: param.account_holder_surname,
-            bank: param.bank,
-            branch: param.bank,
-            branch_code: param.branch_code,
-            type_of_account: param.type_of_account,
-            account_number: param.account_number,
-            method_of_communication:param.method_of_communication, 
-            opt_in_promotional: param.opt_in_promotional,
-            race: param.race,
-            gender: param.gender,
-            qualification: param.qualification,
-            employed: param.empoyed,
-            occupation: param.occupation,
-            how_did_you_hear_about_us: param.how_did_you_hear_about_us,
-            reasons_for_subscribing: param.reasons_for_subscribing,
-            how_did_you_hear_about_us_other: param.how_did_you_hear_about_us_other,
-            topic_interest: param.topic_interest,
-            referredby: param.referredby,
-            referredby_firstname: param.referredby_firstname,
-            referredby_surname: param.referredby_surname,
-            referral_code: param.referral_code,
-            referredby_email: param.referredby_email,
-            referredby_mobile_number: param.referredby_mobile_number,
-            refer_friend: param.refer_friend,
-            center_to_assist: param.center_to_assist,
-            earn_cash_as_ambassador: param.earn_cash_as_ambassador,
-            certificate: param.certificate,
-            highest_qualication_certificate: param.highest_qualication_certificate,
-            bank_proof: param.bank_proof,
-            pop: param.pop,
-            authname: param.firstname +' '+param.surname,
-            signature: param.signature,
-            signed_place: param.signed_place,
-            signed_on: param.signed_on,
-            role: "subscriber",
-        });
+        
         result = await User.updateMany({_id: param.uid}, [{ $set: {
             firstname: param.firstname,
             surname: param.surname,
@@ -243,43 +180,17 @@ async function subscription(param) {
             town_city: param.town_city,
             province: param.province,
             postal_code: param.postal_code,
-            payment_option: param.payment_option,
-            account_holder_title: param.account_holder_title,
-            account_holder_name: param.account_holder_name,
-            account_holder_surname: param.account_holder_surname,
-            bank: param.bank,
-            branch: param.branch,
-            branch_code: param.branch_code,
-            type_of_account: param.type_of_account,
-            account_number: param.account_number,
             method_of_communication:param.method_of_communication, 
             opt_in_promotional: param.opt_in_promotional,
             race: param.race,
             gender: param.gender,
             qualification: param.qualification,
-            employed: param.empoyed,
-            occupation: param.occupation,
             how_did_you_hear_about_us: param.how_did_you_hear_about_us,
-            reasons_for_subscribing: param.reasons_for_subscribing,
-            how_did_you_hear_about_us_other: param.how_did_you_hear_about_us_other,
-            topic_interest: param.topic_interest,
-            referredby: param.referredby,
-            referredby_firstname: param.referredby_firstname,
-            referredby_surname: param.referredby_surname,
-            referral_code: param.referral_code,
-            referredby_email: param.referredby_email,
-            referredby_mobile_number: param.referredby_mobile_number,
-            refer_friend: param.refer_friend,
-            center_to_assist: param.center_to_assist,
-            earn_cash_as_ambassador: param.earn_cash_as_ambassador,
-            certificate: param.certificate,
-            highest_qualication_certificate: param.highest_qualication_certificate,
-            bank_proof: param.bank_proof,
-            pop: param.pop,
+            opt_in_promotional: param.opt_in_promotional,
             authname: param.firstname +' '+param.surname,
-            signature: param.signature,
-            signed_place: param.signed_place,
-            signed_on: param.signed_on,
+            privacy: param.privacy,
+            deals_promotion: param.deals_promotion,
+            in_loop:param.in_loop,
             role: "subscriber",
         } }])
         //console.log('user',result);
