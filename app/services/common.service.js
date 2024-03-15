92295f18c2541345b573cf80482342f7de86d290
Upendra <upendrapanwar456@gmail.com>
2024-03-15 15:10:08 +0530

    Update cancel payfast payment

--- app/services/common.service.js
+++ app/services/common.service.js
@@ -981,8 +981,8 @@ async function cancelPayfastPayment(req) {
     console.log("timestamp", timestamp);
     console.log("signature", signature);
 
-    // const url = `https://sandbox.payfast.co.za/subscriptions/${token}/cancel`;
-    const url = `https://api.payfast.co.za/subscriptions/${token}/cancel?testing=true`;
+    const url = `https://sandbox.payfast.co.za/subscriptions/${token}/cancel`;
+    // const url = `https://api.payfast.co.za/subscriptions/${token}/cancel?testing=true`;
     const version = 'v1';
 
     const headers = {
