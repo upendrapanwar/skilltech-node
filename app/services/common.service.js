236a2242402ab64f97061176fff93ba673f5e413
Upendra <upendrapanwar456@gmail.com>
2024-04-09 18:55:04 +0530

    Update payfast cancel subscription API

--- app/services/common.service.js
+++ app/services/common.service.js
@@ -1095,30 +1095,38 @@ async function cancelPayfastPayment(req) {
   }
 
   function generateSignature() {
-        let pfOutput = "";
-      var data = merchantData.merchantData;
-      var passPhrase = 'quorum87ax36Revving';
-      for (let key in data) {
+    let pfOutput = "";
+    const data = merchantData.merchantData;
+    const passPhrase = 'quorum87ax36Revving';
+
+    // Add merchant-id to the output
+    pfOutput += `merchant-id=${data.merchant_id}&`;
+
+    for (const key in data) {
         if (data.hasOwnProperty(key)) {
-          if (data[key] !== "") {
-            pfOutput += `${key}=${encodeURIComponent(data[key]).replace(
-              /%20/g,
-              "+"
-            )}&`;
-          }
+            if (data[key] !== "" && key !== "merchant_id") { // Exclude merchant_id from signature
+                pfOutput += `${key}=${encodeURIComponent(data[key]).replace(
+                    /%20/g,
+                    "+"
+                )}&`;
+            }
         }
-      }
-      let getString = pfOutput.slice(0, -1);
-      if (passPhrase !== null) {
+    }
+
+    let getString = pfOutput.slice(0, -1); // Remove the trailing '&' from the string
+
+    if (passPhrase.length > 0) {
         getString += `&passphrase=${encodeURIComponent(passPhrase).replace(
-          /%20/g,
-          "+"
+            /%20/g,
+            "+"
         )}`;
-      }
-      const signature = crypto.createHash("md5").update(getString).digest("hex");
-      console.log("signature", signature);
-      return signature;
-      }
+    }
+
+    const signature = crypto.createHash("md5").update(getString).digest("hex");
+    console.log("signature", signature);
+    return signature;
+}
+
 
   try {
     const token = merchantData.token;
